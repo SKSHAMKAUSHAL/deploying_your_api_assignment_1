c160c014e77c13e56dfc64c6a8ac102aecb77443
Sksham Kaushal <skshamkaushal@gmail.com>
2025-02-06 11:23:23 +0530

    done some changes in index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { resolve } = require('path');
+
+require('dotenv').config();
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = config;
