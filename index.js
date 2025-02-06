66b323a87079f140b9862a5f3cdaed1ac4f7c1ee
Sksham Kaushal <skshamkaushal@gmail.com>
2025-02-06 11:43:38 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
@@ -9,6 +11,15 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+module.exports = config;
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
